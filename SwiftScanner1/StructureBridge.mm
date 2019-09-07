8c3f2fb580c30e7112ecca209035c0a0261bf82f
tskapek <tskapek@gmail.com>
2019-09-07 08:32:21 -0400

    Added and imported Structure framework

---
+++ SwiftScanner1/StructureBridge.mm
@@ -0,0 +1,10 @@
+//
+//  StructureBridge.m
+//  SwiftScanner1
+//
+//  Created by Tim Skapek on 9/7/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Structure/Structure.h>
