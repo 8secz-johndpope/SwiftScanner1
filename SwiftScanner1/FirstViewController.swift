ea1a509ff4ed81f26502c17708f6d6398c17a1ff
tskapek <tskapek@gmail.com>
2019-09-06 22:31:38 -0400

    Initial Commit

---
+++ SwiftScanner1/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SwiftScanner1
+//
+//  Created by Tim Skapek on 9/6/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftScanner1/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SwiftScanner1
+//
+//  Created by Tim Skapek on 9/6/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
