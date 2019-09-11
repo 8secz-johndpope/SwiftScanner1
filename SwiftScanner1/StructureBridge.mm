b45aeba1f4d45f0e349c043a771f508f62e8e093
tskapek <tskapek@gmail.com>
2019-09-10 23:45:29 -0400

    re-deleted files

--- SwiftScanner1/StructureBridge.mm
+++
@@ -1,9 +0,0 @@
-//
-//  StructureBridge.m
-//  SwiftScanner1
-//
-//  Created by Tim Skapek on 9/10/19.
-//  Copyright © 2019 Protect3d Inc. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
