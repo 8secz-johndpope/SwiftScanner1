71ea0557693dc0bdc3428a239a2ab4cc4495b7ab
tskapek <tskapek@gmail.com>
2019-09-10 23:39:44 -0400

    Start/stop scan buttons on view 2

---
+++ SwiftScanner1/SwiftScanner1-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  SwiftScanner1
+//
+//  Created by Tim Skapek on 9/10/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#define HAS_LIBCXX
+#import "Structure/Structure.h"
+#import "Structure/StructureSLAM.h"
+
+#endif /* Header_h */
