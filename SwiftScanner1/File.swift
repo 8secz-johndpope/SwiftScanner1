a5ce35ca7f43199517d2ad39712ddf19da07564e
tskapek <42814949+tskapek@users.noreply.github.com>
2019-09-10 23:45:09 -0400

    Delete File.swift

--- SwiftScanner1/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  SwiftScanner1
-//
-//  Created by Tim Skapek and Jackson Hubbard on 9/7/19.
-//  Copyright © 2019 Protect3d Inc. All rights reserved.
-//
-
-import Foundation
