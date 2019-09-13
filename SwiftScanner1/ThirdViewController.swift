d3e9f1bba2b0efc772cd322320ca518bfb3847fb
jacksonhubbard <jwh62@duke.edu>
2019-09-13 12:50:21 -0400

    added View-controller for user info

---
+++ SwiftScanner1/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  SwiftScanner1
+//
+//  Created by Jackson Hubbard on 9/13/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
