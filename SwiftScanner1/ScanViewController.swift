a430d1b62887802bd0fb9a17041cc41ad0258f1e
jacksonhubbard <jwh62@duke.edu>
2019-09-13 22:24:17 -0500

    added tab for Structure SDK

--- SwiftScanner1/ScanViewController.swift
+++ SwiftScanner1/ScanViewController.swift
@@ -8,9 +8,32 @@
 
 import UIKit
 
-class ScanViewController: UIViewController {
+class ScanViewController: UIViewController, STSensorControllerDelegate {
 
+    @IBAction func startScanButton(_ sender: Any) {
+    }
+    
+    @IBAction func stopScanButton(_ sender: Any) {
+    }
+    
     @IBOutlet weak var nameLabel: UILabel!
+
+    
+    var sharedController: STSensorController
+    
+    required init?(coder decoder: NSCoder){
+        
+        sharedController = STSensorController.shared()
+        super.init(coder: decoder)
+        
+        sharedController.delegate = self
+        sharedController.initializeSensorConnection()
+    }
+    
+    
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,15 +45,67 @@ class ScanViewController: UIViewController {
         nameLabel.text = "Begin scanning " + athlete
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    // Test button method
+    @objc func startButtonAction(sender: UIButton!){
+        let streamingOptions: [AnyHashable: Any] = [
+            kSTStreamConfigKey: NSNumber(value: STStreamConfig.depth640x480.rawValue as Int),
+            kSTFrameSyncConfigKey: NSNumber(value: STFrameSyncConfig.depthAndRgb.rawValue as Int),
+            kSTHoleFilterEnabledKey: true
+        ]
+        do {
+            try sharedController.startStreaming(options: streamingOptions)
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
-    */
+    @objc func stopButtonAction(sender: UIButton!){
+        sharedController.stopStreaming()
+    }
+    
+    
+    // Necessary implemented Structure methods
+    func sensorDidOutputSynchronizedInfraredFrame(_ irFrame: STInfraredFrame!, colorFrame: STColorFrame!) {
+        
+    }
+    func sensorDidOutputInfraredFrame(_ irFrame: STInfraredFrame!) {
+        
+    }
+    func sensorDidOutputSynchronizedDepthFrame(_ depthFrame: STDepthFrame!, colorFrame: STColorFrame!) {
+        let alignedDepthFrame: STDepthFrame = depthFrame.registered(to: colorFrame)
+        // OWN PROCESSING ON alignedDepthFrame
+    }
+    func sensorDidOutputDepthFrame(_ depthFrame: STDepthFrame!) {
+        
+    }
+    
+    func captureOutput(_output: AVCaptureOutput, didOutput
+        sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection)
+    {
+        //Feed it to Structure sensor
+        sharedController.frameSyncNewColorBuffer(sampleBuffer)
+    }
+    
+    
+    
+    // Necessary methods to derive from STSensorControllerDelegate
+    func sensorDidConnect() {
+    }
+    
+    func sensorDidDisconnect() {
+        
+    }
+    
+    func sensorDidStopStreaming(_ reason: STSensorControllerDidStopStreamingReason) {
+        
+    }
+    
+    func sensorDidLeaveLowPowerMode() {
+        
+    }
+    
+    func sensorBatteryNeedsCharging() {
+        
+    }
 
 }
