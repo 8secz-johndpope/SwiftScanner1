3d0a459758f9b9af375836fc322d2ff071720569
tskapek <tskapek@gmail.com>
2019-09-14 12:08:25 -0500

    Start/stop scanner buttons added to View 2

---
+++ SwiftScanner1/XibView.swift
@@ -0,0 +1,44 @@
+//
+//  SampleScannerTest.swift
+//  SwiftScanner1
+//
+//  Created by Tim Skapek on 9/13/19.
+//  Copyright © 2019 Protect3d Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class XibView: UIView {
+    
+    var contentView: UIView?
+    @IBInspectable var nibName:String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        xibSetup()
+    }
+    
+    func xibSetup(){
+        guard let view  = loadViewFromNib() else { return }
+        view.frame = bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(view)
+        contentView = view
+    }
+    
+    func loadViewFromNib() -> UIView?{
+        guard let nibName = nibName else { return nil}
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        return nib.instantiate(
+            withOwner: self,
+            options: nil).first as? UIView
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        xibSetup()
+        contentView?.prepareForInterfaceBuilder()
+    }
+}
